f9f04caa70b6e30cf7e2944fb48341b8cb9678f0
Shruti Bedarkar <shruti.bedarkar@comprinno.net>
2026-02-27 18:59:28 +0530

    Add all files

---
+++ Longest.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Longest {
+    public static int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        int longest = 0;
+        
+        
+        for (int num : set) {
+        
+            if (!set.contains(num - 1)) {
+                
+                int currentNum = num;
+                int count = 1;
+             
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+                
+                longest = Math.max(longest, count);
+            }
+        }
+        
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.println(longestConsecutive(arr));
+    }
+}
---
+++ Top.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Top {
+
+    public static int[] topKFrequent(int[] nums, int k) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(a) - map.get(b)
+        );
+
+        for(int num : map.keySet()) {
+            pq.add(num);
+            if(pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        
+        int[] result = new int[k];
+        for(int i = k - 1; i >= 0; i--) {
+            result[i] = pq.poll();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        int[] ans = topKFrequent(nums, k);
+
+        for(int num : ans) {
+            System.out.print(num + " ");
+        }
+    }
+}
